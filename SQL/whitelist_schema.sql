bcac89689523dc5baed2d67cbcfdbe680d084862
Марк <chuykov.andrey.2001@gmail.com>
2025-04-01 05:56:49 +0300

    +

--- SQL/whitelist_schema.sql
+++ SQL/whitelist_schema.sql
@@ -1,82 +1,80 @@
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
-
 --
--- Table structure for table `player_rank`.
+-- Table structure for table `whitelist`.
 --
-DROP TABLE IF EXISTS `player_rank`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
+DROP TABLE IF EXISTS `whitelist_temp`;
+--
+CREATE TABLE `whitelist_temp` AS SELECT `id`, `ckey` FROM `whitelist`;
+--
+DROP TABLE IF EXISTS `whitelist`;
+/*!40101 SET @saved_cs_client = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `player_rank` (
-  `ckey` VARCHAR(32) NOT NULL,
-  `rank` VARCHAR(12) NOT NULL,
-  `admin_ckey` VARCHAR(32) NOT NULL,
-  `date_added` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `last_modified` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `deleted` BOOLEAN NOT NULL DEFAULT FALSE,
-  PRIMARY KEY (`ckey`, `rank`)
+CREATE TABLE `whitelist` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `ckey` varchar(32) NOT NULL,
+  `manager` VARCHAR(32) NOT NULL,
+  `manager_id` VARCHAR(32) NOT NULL,
+  `date_added` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+  `last_modified` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP() ON UPDATE CURRENT_TIMESTAMP(),
+  `deleted` tinyint(1) NOT NULL DEFAULT 0,
+	`comment` VARCHAR(4000),
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `unique_ckey` (`ckey`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+--
+INSERT INTO `whitelist` (`id`, `ckey`, `manager`, `manager_id`, `comment`)
+SELECT `id`, `ckey`, 'SQL Import Script', '0', 'SQL Import Script' FROM `whitelist_temp`;
+--
+SELECT IFNULL(MAX(`id`), 0) + 1 INTO @next_auto_increment FROM `whitelist`;
+SET @alter_sql = CONCAT('ALTER TABLE `whitelist` AUTO_INCREMENT = ', @next_auto_increment, ';');
+PREPARE stmt FROM @alter_sql;
+EXECUTE stmt;
+DEALLOCATE PREPARE stmt;
+--
+DROP TABLE IF EXISTS `whitelist_temp`;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
-
 --
--- Table structure for table `player_rank_log`.
+-- Table structure for table `whitelist_log`.
 --
-DROP TABLE IF EXISTS `player_rank_log`;
+DROP TABLE IF EXISTS `whitelist_log`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `player_rank_log` (
+CREATE TABLE `whitelist_log` (
   `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
   `ckey` VARCHAR(32) NOT NULL,
-  `rank` VARCHAR(12) NOT NULL,
-  `admin_ckey` VARCHAR(32) NOT NULL,
+	`manager` VARCHAR(32) NOT NULL,
+	`manager_id` VARCHAR(32) NOT NULL,
   `action` ENUM('ADDED', 'REMOVED') NOT NULL DEFAULT 'ADDED',
   `date` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	`comment` VARCHAR(4000),
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 
 --
--- Trigger structure for trigger `log_player_rank_additions`.
+-- Trigger structure for trigger `log_whitelist_additions`.
 --
-DROP TRIGGER IF EXISTS `log_player_rank_additions`;
-CREATE TRIGGER `log_player_rank_additions`
-AFTER INSERT ON `player_rank`
+DROP TRIGGER IF EXISTS `log_whitelist_additions`;
+CREATE TRIGGER `log_whitelist_additions`
+AFTER INSERT ON `whitelist`
 FOR EACH ROW
-INSERT INTO player_rank_log (ckey, rank, admin_ckey, `action`) VALUES (NEW.ckey, NEW.rank, NEW.admin_ckey, 'ADDED');
+INSERT INTO whitelist_log (ckey, manager, manager_id, `action`, comment) VALUES (NEW.ckey, NEW.manager, NEW.manager_id, 'ADDED', NEW.comment);
 
 
 --
--- Trigger structure for trigger `log_player_rank_changes`.
+-- Trigger structure for trigger `log_whitelist_changes`.
 --
-DROP TRIGGER IF EXISTS `log_player_rank_changes`;
+DROP TRIGGER IF EXISTS `log_whitelist_changes`;
 DELIMITER //
-CREATE TRIGGER `log_player_rank_changes`
-AFTER UPDATE ON `player_rank`
+CREATE TRIGGER `log_whitelist_changes`
+AFTER UPDATE ON `whitelist`
 FOR EACH ROW
 BEGIN
  IF NEW.deleted = 1 THEN
-  INSERT INTO player_rank_log (ckey, rank, admin_ckey, `action`) VALUES (NEW.ckey, NEW.rank, NEW.admin_ckey, 'REMOVED');
+  INSERT INTO whitelist_log (ckey, manager, manager_id, `action`, comment) VALUES (NEW.ckey, NEW.manager, NEW.manager_id, 'REMOVED', NEW.comment);
  ELSE
-  INSERT INTO player_rank_log (ckey, rank, admin_ckey, `action`) VALUES (NEW.ckey, NEW.rank, NEW.admin_ckey, 'ADDED');
+  INSERT INTO whitelist_log (ckey, manager, manager_id, `action`, comment) VALUES (NEW.ckey, NEW.manager, NEW.manager_id, 'ADDED', NEW.comment);
  END IF;
 END; //
 DELIMITER ;
-
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
